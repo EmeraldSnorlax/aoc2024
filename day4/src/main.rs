ef87c6fb8e5aa26d47af81c6c0e451e5b5dcebd3
Rain <emeraldsnorlax@disroot.org>
2024-12-04 17:23:06 +0000

    day 4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -7,17 +7,20 @@ fn main() {
         .map(|line| line.chars().collect())
         .collect();
 
-    let mut count = 0;
+    let mut xmas_count = 0;
+    let mut x_mas_count = 0;
 
     for _ in 0..4 {
         for i in 0..grid.len() {
             for j in 0..grid[i].len() {
-                count += search_east_and_southeast_for_xmas((i, j), &grid);
+                xmas_count += search_east_and_southeast_for_xmas((i, j), &grid);
+                x_mas_count += search_for_x_mas((i, j), &grid);
             }
         }
         grid = rotate(grid);
     }
-    print!("XMAS: {}", count);
+    println!("XMAS: {}", xmas_count);
+    println!("X-MAS: {}", x_mas_count);
 }
 
 fn search_east_and_southeast_for_xmas(pos: (usize, usize), grid: &Vec<Vec<char>>) -> i32 {
@@ -37,7 +40,6 @@ fn search_east_and_southeast_for_xmas(pos: (usize, usize), grid: &Vec<Vec<char>>
     }
     if discovered == "XMAS" {
         count += 1;
-        println!("XMAS");
     }
 
     // Just check for SE direction match
@@ -52,12 +54,27 @@ fn search_east_and_southeast_for_xmas(pos: (usize, usize), grid: &Vec<Vec<char>>
     }
     if discovered == "XMAS" {
         count += 1;
-        println!("XMAS diag");
     }
 
     count
 }
 
+fn search_for_x_mas(pos: (usize, usize), grid: &Vec<Vec<char>>) -> i32 {
+    if pos.0 + 2 >= grid.len() || pos.1 + 2 >= grid[pos.0].len() {
+        return 0;
+    }
+
+    if grid[pos.0][pos.1] == 'M'
+        && grid[pos.0 + 2][pos.1] == 'M'
+        && grid[pos.0 + 1][pos.1 + 1] == 'A'
+        && grid[pos.0][pos.1 + 2] == 'S'
+        && grid[pos.0 + 2][pos.1 + 2] == 'S'
+    {
+        return 1;
+    }
+
+    0
+}
 fn rotate(grid: Vec<Vec<char>>) -> Vec<Vec<char>> {
     let mut new_grid = vec![vec![' '; grid.len()]; grid[0].len()];
     for i in 0..grid.len() {
@@ -111,6 +128,26 @@ mod tests {
             vec!['B', '7', '3'],
             vec!['C', '8', '4'],
         ];
-        assert_eq!(rotate(&grid), rotated);
+        assert_eq!(rotate(grid), rotated);
+    }
+
+    #[test]
+    fn test_search_for_x_mas() {
+        let grid = vec![
+            vec!['M', 'M', 'S', 'S'],
+            vec!['M', 'A', 'A', 'S'],
+            vec!['M', 'S', 'S', 'S'],
+            vec!['S', 'A', 'S', 'X'],
+        ];
+        assert_eq!(search_for_x_mas((0, 0), &grid), 1);
+
+        let grid = vec![
+            vec!['X', 'M', 'A', 'S'],
+            vec!['M', 'M', 'A', 'S'],
+            vec!['A', 'A', 'A', 'S'],
+            vec!['S', 'A', 'S', 'S'],
+        ];
+        assert_eq!(search_for_x_mas((0, 0), &grid), 0);
+        assert_eq!(search_for_x_mas((1, 1), &grid), 0);
     }
 }
