730ebb429b0f2311dff8b5cc6d68fe9763cbd9a4
Rain <emeraldsnorlax@disroot.org>
2024-12-06 00:38:24 +0000

    day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -9,19 +9,36 @@ fn main() {
 
     let valid_jobs: Vec<&Vec<u8>> = jobs
         .iter()
-        .filter(|job| is_job_valid(&rules, job))
+        .filter(|job| job_error_index(&rules, job).is_none())
         .collect();
 
-    let mut total_of_middle_pages: u32 = 0;
-    for job in valid_jobs {
-        total_of_middle_pages += job[job.len() / 2] as u32;
+    println!(
+        "Total of middle pages: {}",
+        total_of_middle_pages(valid_jobs)
+    );
+
+    // Part 2
+    let mut invalid_jobs: Vec<Vec<u8>> = jobs
+        .iter()
+        .cloned()
+        .filter(|job| job_error_index(&rules, job).is_some())
+        .collect();
+
+    for job in &mut invalid_jobs {
+        while job_error_index(&rules, &job).is_some() {
+            let i = job_error_index(&rules, &job).unwrap();
+            let swap = job[i];
+            job[i] = job[i - 1];
+            job[i - 1] = swap;
+        }
     }
-    println!("Total of middle pages: {}", total_of_middle_pages);
+    println!(
+        "Total of invalid jobs' middle pages after fixing: {}",
+        total_of_middle_pages(invalid_jobs.iter().collect())
+    );
 }
 
-fn is_job_valid(rules: &HashMap<u8, Vec<u8>>, job: &Vec<u8>) -> bool {
-    let mut valid = true;
-
+fn job_error_index(rules: &HashMap<u8, Vec<u8>>, job: &Vec<u8>) -> Option<usize> {
     'page: for page_i in 0..job.len() {
         let page = job[page_i];
         if !rules.contains_key(&page) {
@@ -34,13 +51,12 @@ fn is_job_valid(rules: &HashMap<u8, Vec<u8>>, job: &Vec<u8>) -> bool {
             // Check if the page_rule appears after the current page by scanning after page_i
             for i in 0..page_i {
                 if job[i] == *page_rule {
-                    valid = false;
-                    break 'page;
+                    return Some(page_i);
                 }
             }
         }
     }
-    valid
+    None
 }
 
 fn parse_input(lines: Vec<&str>) -> (HashMap<u8, Vec<u8>>, Vec<Vec<u8>>) {
@@ -60,6 +76,14 @@ fn parse_input(lines: Vec<&str>) -> (HashMap<u8, Vec<u8>>, Vec<Vec<u8>>) {
     (rules, jobs)
 }
 
+fn total_of_middle_pages(valid_jobs: Vec<&Vec<u8>>) -> u32 {
+    let mut total_of_middle_pages: u32 = 0;
+    for job in valid_jobs {
+        total_of_middle_pages += job[job.len() / 2] as u32;
+    }
+    total_of_middle_pages
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -72,11 +96,11 @@ mod tests {
 
         println!("{:?}", rules);
         println!("{:?}", jobs[0]);
-        assert_eq!(is_job_valid(&rules, &jobs[0]), true);
-        assert_eq!(is_job_valid(&rules, &jobs[1]), true);
-        assert_eq!(is_job_valid(&rules, &jobs[2]), true);
-        assert_eq!(is_job_valid(&rules, &jobs[3]), false);
-        assert_eq!(is_job_valid(&rules, &jobs[4]), false);
-        assert_eq!(is_job_valid(&rules, &jobs[5]), false);
+        assert!(job_error_index(&rules, &jobs[0]).is_none());
+        assert!(job_error_index(&rules, &jobs[2]).is_none());
+        assert!(job_error_index(&rules, &jobs[1]).is_none());
+        assert!(job_error_index(&rules, &jobs[3]).is_some());
+        assert!(job_error_index(&rules, &jobs[4]).is_some());
+        assert!(job_error_index(&rules, &jobs[5]).is_some());
     }
 }
