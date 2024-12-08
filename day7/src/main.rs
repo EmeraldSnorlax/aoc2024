edba00a651694bdd0e4b273e6fdea90482590172
Rain <emeraldsnorlax@disroot.org>
2024-12-08 01:24:17 +0000

    day 7

---
+++ day7/src/main.rs
@@ -0,0 +1,52 @@
+use itertools::{repeat_n, Itertools};
+use std::fs;
+
+#[derive(Debug)]
+struct PotentialCalculation {
+    total: i64,
+    inputs: Vec<i64>,
+}
+
+fn add(left: i64, right: i64) -> i64 {
+    left + right
+}
+
+fn multiply(left: i64, right: i64) -> i64 {
+    left * right
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let calculations: Vec<PotentialCalculation> = input
+        .lines()
+        .map(|line| PotentialCalculation {
+            total: line.split(": ").next().unwrap().parse().unwrap(),
+            inputs: line
+                .split(": ")
+                .nth(1)
+                .unwrap()
+                .split(" ")
+                .map(|x| x.parse().unwrap())
+                .collect(),
+        })
+        .collect();
+
+    let mut total_of_valid: i64 = 0;
+
+    let possible_operations = [add, multiply];
+    'calculation: for calculation in calculations {
+        let perms: Vec<Vec<fn(i64, i64) -> i64>> = repeat_n(possible_operations, calculation.inputs.len() - 1).multi_cartesian_product().collect();
+        for perm in perms {
+            let mut total = calculation.inputs[0];
+            for i in 0..perm.len() {
+                total = perm[i](total, calculation.inputs[i + 1]);
+            }
+            if total == calculation.total {
+                total_of_valid += total;
+                continue 'calculation;
+            }
+        }
+    }
+
+    println!("Total of valid: {:?}", total_of_valid);
+}
